1c5e521d9ec2367627c36482ede805eb2757fe87
artamonovalex18 <artamonovalex18@gmail.com>
2019-03-20 23:30:09 +0300

    Changed encryptor

--- src/main/java/ipm/customs/IAD4_BACKEND/Services/Encryptor.java
+++ src/main/java/ipm/customs/IAD4_BACKEND/Services/Encryptor.java
@@ -1,9 +1,10 @@
 package ipm.customs.IAD4_BACKEND.Services;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+//import sun.misc.BASE64Decoder;
+//import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
+import java.util.Base64;
 import java.util.Random;
 
 public class Encryptor {
@@ -11,19 +12,21 @@ public class Encryptor {
     private static Random rand = new Random(5);
 
     public static String encrypt(String str) {
-        BASE64Encoder encoder = new BASE64Encoder();
+        Base64.Encoder encoder = Base64.getEncoder();
+//        BASE64Encoder encoder = new BASE64Encoder();
         byte[] salt = new byte[8];
         rand.nextBytes(salt);
 
-        return encoder.encode(salt) + encoder.encode(str.getBytes());
+        return encoder.encodeToString(salt) + encoder.encodeToString(str.getBytes());
     }
     public static String decrypt(String encstr) {
         if (encstr.length() > 12) {
             String cipher = encstr.substring(12);
-            BASE64Decoder decoder = new BASE64Decoder();
+            Base64.Decoder decoder = Base64.getDecoder();
+//            BASE64Decoder decoder = new BASE64Decoder();
             try {
-                return new String(decoder.decodeBuffer(cipher));
-            } catch (IOException e) {
+                return new String(decoder.decode(cipher));
+            } catch (Exception e) {
                 //  throw new InvalidImplementationException(
                 //Fail
             }
